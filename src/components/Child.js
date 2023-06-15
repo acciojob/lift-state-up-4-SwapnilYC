a014b45b6c07128ae3646e2a38980ab72044e711
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-15 07:35:36 +0000

    if list exist then only child rendered cond added

--- src/components/Child.js
+++ src/components/Child.js
@@ -5,13 +5,13 @@ function Child({list}) {
 return(
     <div>
         <ul>
-        {
-            list.map( (item, i) => (
+        {list &&
+            (list.map( (item, i) => (
                 <> 
                 <li id={i}>{item.ItemName}-{item.ItemPrice}</li> <button onClick={()=> removeChild(i)}>Remove</button>
                 </>
                
-            ))
+            )))
         }
         </ul>
     </div>
